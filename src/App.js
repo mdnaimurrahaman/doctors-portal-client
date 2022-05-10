b5dece27625a932ad42a040ee01c0ad410e9edf5
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-05-10 18:55:25 +0600

    Creating components and setting up raouts

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
+import ContactUs from './Pages/ContactUs/ContactUs';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Reviews from './Pages/Reviews/Reviews';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -11,10 +15,15 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}/>
+        <Route path='/appointment' element={<Appointment></Appointment>}/>
+        <Route path='/reviews' element={<Reviews></Reviews>}/>
+        <Route path='contactUs' element={<ContactUs></ContactUs>}/>
         <Route path='about' element={<About></About>}/>
+        <Route path='login' element={<Login></Login>}/>
       </Routes>
-    </div>
+    </div> 
   );
 }
 
 export default App;
+ 
