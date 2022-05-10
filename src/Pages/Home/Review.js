03f71bd1cb382377a45a8a47bf3b743f10192604
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-05-11 01:16:07 +0600

    added review section

---
+++ src/Pages/Home/Review.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Review = ({ review }) => {
+  return (
+    <div class="card lg:max-w-lg bg-base-100 shadow-xl">
+      <div class="card-body">
+        <h2 class="card-title">Card title!</h2>
+        <p>
+          It is a long established fact that by the readable content of a lot
+          layout. The point of using Lorem a more-or-less normal distribu to
+          using Content here, content
+        </p>
+      </div>
+      <div className="flex items-center justify-center mb-7">
+        <div class="avatar">
+          <div class="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 mr-5">
+            <img src={review.img} alt=""/>
+          </div>
+        </div>
+        <div>
+            <h4 className="text-xl">{review.name}</h4>
+            <p>{review.location}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Review;
