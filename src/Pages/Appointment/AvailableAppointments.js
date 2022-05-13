654523e54532be059373a82f7ebd8c1c7b2df26c
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-05-14 02:14:46 +0600

    Booking modal added

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,9 +1,11 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import BookingModal from './BookingModal';
 import BookService from './BookService';
 
 const AvailableAppointments = ({date}) => {
     const [services, setServices] = useState([]);
+    const [treatment, setTreatment] = useState([]);
 
     useEffect(() => {
         fetch('services.json')
@@ -15,7 +17,10 @@ const AvailableAppointments = ({date}) => {
             <h4 className='text-center text-xl text-secondary mt-5'>Available Appointments on {format(date,'PP')}</h4>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-20'>
                 {
-                    services.map(service => <BookService key={service._id} service={service}></BookService>)
+                    services.map(service => <BookService key={service._id} service={service} setTreatment={setTreatment}></BookService>)
+                }
+                {
+                    treatment && <BookingModal treatment={treatment}></BookingModal>
                 }
             </div>
         </div>
